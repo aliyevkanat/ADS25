deee7efa684c91a2ea0346504af19fdc4fbe6c3f
Алиев Канат <alievkanat@MacBook-Pro-5.local>
2025-09-26 16:34:23 +0500

    Lab2

--- Labs/Lab2/F.cpp
+++ Labs/Lab2/F.cpp
@@ -1,28 +1,84 @@
 #include <iostream>
 using namespace std;
 
-class Node{
-    int data;
-    Node *next;
+struct Node {
+    int data;  
+    Node* next;  
 
-    Node(int x){
+    Node(int x) {  
         data = x;
         next = nullptr;
     }
 };
 
+struct ins {
+    private:
+        Node* head;  
+        int len;   
 
+    public:
+        ins() {  
+            head = nullptr;
+            len = 0;
+        }
 
-int main(){
+        void insert(int value) {
+            Node* newNode = new Node(value);  
+            if (len == 0) {
+                head = newNode;  
+            } else {
+                Node* temp = head;
+                while (temp->next != nullptr) {
+                    temp = temp->next;  
+                }
+                temp->next = newNode; 
+            }
+            len++;
+        }
 
-int n;
-cin >> n;
-int x;
-for(int i = 0;i < n;i++){
-    cin >> x;
+        void insert_at_position(int value, int position) {
+            Node* newNode = new Node(value);
+            if (position == 0) {
+                newNode->next = head;
+                head = newNode;
+            } else {
+                Node* temp = head;
+                for (int i = 0; i < position - 1 && temp != nullptr; i++) {
+                    temp = temp->next;  
+                }
+                if (temp != nullptr) {
+                    newNode->next = temp->next;
+                    temp->next = newNode;
+                }
+            }
+            len++;
+        }
 
-}
+      
+        void print() {
+            Node* temp = head;
+            while (temp != nullptr) {
+                cout << temp->data << " ";
+                temp = temp->next;
+            }
+            cout << endl;
+        }
+};
 
+int main() {
+    int n; cin >> n;
+    ins list;  
 
-    return 0;
+   
+    for (int i = 0; i < n; i++) {
+        int k; cin >> k;
+        list.insert(k); 
+    }
+
+    int valueToInsert, position; cin >> valueToInsert >> position; 
+
+    list.insert_at_position(valueToInsert, position);  
+
+    list.print();  
+
 }
