9b4edcaab59287da34d9822d0da4514853ab6a96
Алиев Канат <alievkanat@MacBook-Pro-5.local>
2025-09-26 23:10:20 +0500

    K.cpp

---
+++ Labs/Lab3/K.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cmath>
+#include <climits>
+using namespace std;
+#define ll long long
+const int N = 1e5+2;
+int main(){
+
+ll n,k,min = INT_MAX;
+ll a[N];
+ll pref[N];
+
+for(int i = 0;i < n;i++){
+    cin >> a[i];
+    pref[i] = pref[i-1] + a[i];
+}
+    int l = 1;
+    int r = n;
+    int answer = n;
+
+while(l <= r){
+    ll mid = l + (r - l) / 2;
+    bool found = false;
+
+    for(int i = 1;i <= n - mid + 1;i++){
+        ll sum = pref[i + mid - 1] - pref[i - 1];
+        if(sum >= k){
+            found = true;
+            break;
+        }
+    }
+    if(found){
+    answer = mid;
+    r = mid - 1;
+}else l = mid + 1;
+
+}
+cout << answer;
+    return 0;
+}
